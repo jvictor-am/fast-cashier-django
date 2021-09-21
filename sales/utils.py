43e0add51ecd180b643bfc7000c83940808c306e
jvictor-am <joaovictorengp@gmail.com>
2021-09-21 19:08:18 -0300

    adjust order_total_commission function

--- sales/utils.py
+++ sales/utils.py
@@ -1,5 +1,9 @@
 # python imports
 import datetime
+import decimal
+
+# django imports
+from django.utils import timezone
 
 # local imports
 from.models import OrderItem
@@ -22,12 +26,25 @@ def order_total_commission(obj):
     response = []
 
     for item in OrderItem.objects.filter(order=obj):
-        commission = ''
-        if item.product.variable_commission_percentage:
-            commission = item.product.variable_commission_percentage
-        else:
-            commission = item.product.fixed_commission_percentage
 
-        response.append((commission/100)*item.product.price*item.quantity)
+        created_data_from_model = item.order.created # it comes with UTC timezone (-3 hours)
+        created_data_adjusted_to_local_time = timezone.localtime(created_data_from_model)
+
+        midnight_same_day = created_data_adjusted_to_local_time.replace(hour=0, minute=0, second=0, microsecond=0)
+        before_midnight_next_day = created_data_adjusted_to_local_time.replace(hour=23, minute=59, second=59, microsecond=59)
+        noon = created_data_adjusted_to_local_time.replace(hour=12, minute=0, second=0, microsecond=0)
+
+        commission = item.product.fixed_commission_percentage
+
+        if (midnight_same_day<=created_data_adjusted_to_local_time<=noon) and item.product.fixed_commission_percentage > 5:
+            item.product.variable_commission_percentage = 5
+            item.product.save(update_fields=['variable_commission_percentage'])
+            commission = item.product.variable_commission_percentage
+        if (noon<created_data_adjusted_to_local_time<before_midnight_next_day) and item.product.fixed_commission_percentage < 4:
+            item.product.variable_commission_percentage = 4
+            item.product.save(update_fields=['variable_commission_percentage'])
+            commission = item.product.variable_commission_percentage
+
+        response.append(decimal.Decimal(commission/100)*item.product.price*item.quantity)
 
     return sum(response)
