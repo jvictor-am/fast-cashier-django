3d46351606607c9cd1e432fa917d08a7edbcb92e
jvictor-am <joaovictorengp@gmail.com>
2021-09-21 04:37:35 -0300

    adjust admin django system to basic usage

---
+++ sales/admin.py
@@ -0,0 +1,28 @@
+# django imports
+from django.contrib import admin
+
+# local imports
+from .models import Product
+from .models import Customer
+from .models import Seller
+from .models import Order
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'fixed_commission_percentage')
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'seller', 'customer')
